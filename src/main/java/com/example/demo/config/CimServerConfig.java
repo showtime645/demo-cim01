9d81acc564a1d57b726fff19f61be34d565b855f
Gress Su (额布苏) <ebusu@inspur.com>
2018-07-28 15:06:48 +0800

    ctrl + shift o

--- src/main/java/com/example/demo/config/CimServerConfig.java
+++ src/main/java/com/example/demo/config/CimServerConfig.java
@@ -1,7 +1,6 @@
 package com.example.demo.config;
 
 import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
